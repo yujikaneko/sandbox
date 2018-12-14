27755dc4e824c73f285ba9d76f154afcf1be8634
Yuji Kaneko <kaneko@win2.tokyo>
2018-12-14 16:35:46 +0900

    ref

--- opencv/opencv.cpp
+++ opencv/opencv.cpp
@@ -1,96 +1,132 @@
-//============================================================================
-// Name        : opencv.cpp
-// Author      :
-// Version     :
-// Copyright   : Your copyright notice
-// Description : https://qiita.com/mikaji/items/3e3f85e93d894b4645f7
-//============================================================================
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-//cv::Mat affine(cv::Mat src, int x, int y) {
-//    cv::Mat mat =
-//        (cv::Mat_<float>(2, 3) << 1.0, 0.0, (float)x, 0.0, 1.0, (float)y);
-//    cv::Mat dst;
-//    cv::warpAffine(src, dst, mat, src.size());
-//    return dst;
-//}
-
 const int cell_size = 5;
 const int block_size = 5;
+const int block_area = block_size * block_size;
 const int bin_num = 4;
 
-cv::Mat HistCell(cv::Mat grad, cv::Mat angle, int x, int y) {
-    int X = x + cell_size;
-    int Y = y + cell_size;
+// 1セルの Histogram を算出
+cv::Mat HistCell(cv::Mat grad, cv::Mat angle) {
     cv::Size siz(bin_num, 1);
-    cv::Mat dst = cv::Mat::zeros(siz, CV_32F);
-    for (; y < Y; y++) {
-        for (; x < X; x++) {
+    cv::Mat hist = cv::Mat::zeros(1, 1, CV_32FC(bin_num));
+    for (int y = 0; y < cell_size; y++) {
+        for (int x = 0; x < cell_size; x++) {
             float theta = angle.at<float>(y, x);
             unsigned int th_id =
                 static_cast<unsigned int>(theta / 360.0 * bin_num * 4.0);
             th_id = ((th_id + 1) / 2) % bin_num;
-            dst.at<float>(0, th_id) += grad.at<float>(y, x);
+            hist.data[th_id] += grad.at<float>(y, x);
         }
     }
-    return dst;
-}
-
-cv::Mat HOG(cv::Mat grad, cv::Mat angle) {
-    int X = grad.cols;
-    int Y = grad.rows;
-    cv::Size cell_num(static_cast<int>(X / cell_size),
-                      static_cast<int>(Y / cell_size));
-    cv::Size hist_siz(bin_num * cell_num.width, cell_num.height);
-    cv::Mat hist = cv::Mat::zeros(hist_siz, CV_32F);
-
-    cv::Rect roi(1, 1, cell_size, cell_size);
-    cv::Rect dst_roi(0, 0, bin_num, 1);
-    for (roi.y = 1; roi.y + cell_size < Y - 1; roi.y += cell_size) {
-        for (roi.x = 1; roi.x + cell_size < X - 1; roi.x += cell_size) {
-            cv::Mat h = HistCell(grad, angle, roi.x, roi.y);
-            h.copyTo(hist(dst_roi));
-            dst_roi.x += bin_num;
-        }
-        dst_roi.x = 0;
-        dst_roi.y++;
-    }
-
-
     return hist;
 }
 
-int main(void) {
-    cv::Mat src_img = cv::imread("/home/amano/sample.jpg", 1);
-    if (src_img.empty()) return -1;
+// Histogram of Gradient 算出
+cv::Mat HOG(cv::Mat img_src) {
+    std::cout << "HOG Start!" << std::endl;
+    std::cout << " Input size:" << img_src.size() << " ch=["
+              << img_src.channels() << "]" << std::endl;
+    std::cout << " cell_size:" << cell_size << ",block_size:" << block_size
+              << ",bin_num:" << bin_num << std::endl;
+    std::cout << " feature vector dim:" << block_area * bin_num << std::endl;
 
-    cv::Mat m_img, mono_img;
-    cv::cvtColor(src_img, m_img, CV_RGB2GRAY);
-    m_img.convertTo(mono_img, CV_32F);
+    // グレースケールに変換
+    cv::Mat img_mono, img_mono_f;
+    cv::cvtColor(img_src, img_mono, CV_RGB2GRAY);
+    img_mono.convertTo(img_mono_f, CV_32F);
+    std::cout << " converted to gray scale" << std::endl;
 
+    // 微分画像作成
     const cv::Mat ker_x =
         (cv::Mat_<float>(3, 3) << 0, 0, 0, -0.5, 0, 0.5, 0, 0, 0);
     const cv::Mat ker_y =
         (cv::Mat_<float>(3, 3) << 0, -0.5, 0, 0, 0, 0, 0, 0.5, 0);
     cv::Mat gradx, grady;
-    cv::filter2D(mono_img, gradx, -1, ker_x);
-    cv::filter2D(mono_img, grady, -1, ker_y);
+    cv::filter2D(img_mono_f, gradx, -1, ker_x);
+    cv::filter2D(img_mono_f, grady, -1, ker_y);
+    std::cout << " created deliverable image" << std::endl;
 
-    //    cv::Mat img_xp1 = affine(mono_img, 1, 0);
-    //    cv::Mat img_xm1 = affine(mono_img, -1, 0);
-    //    cv::Mat img_yp1 = affine(mono_img, 0, 1);
-    //    cv::Mat img_ym1 = affine(mono_img, 0, -1);
+    // 極座標変換
+    cv::Mat grad, angl;
+    cv::cartToPolar(gradx, grady, grad, angl, true);
+    std::cout << " calculated gradient and angle" << std::endl;
 
-    //    cv::Mat gradx = img_xp1 - img_xm1;
-    //    cv::Mat grady = img_yp1 - img_ym1;
-    cv::Mat mag, angl;
-    cv::cartToPolar(gradx, grady, mag, angl, true);
-    cv::Mat hog = HOG(mag, angl);
-    // std::cout << "hog=" << hog << std::endl;
+    // セルごとのヒストグラム作成
+    std::cout << " start histogram calculation" << std::endl;
+    int X = grad.cols;
+    int Y = grad.rows;
+    cv::Size cell_num(X / cell_size, Y / cell_size);
+    cv::Rect cell_roi(0, 0, cell_size, cell_size);
+    cv::Mat hist =
+        cv::Mat::zeros(cell_num.height, cell_num.width, CV_32FC(bin_num));
+    cv::Rect hist_roi(0, 0, 1, 1);
+    for (int y = 0; y < Y - cell_size; y += cell_size) {
+        cell_roi.y = y;
+        if (y % 100 == 0) {
+            std::cout << " " << y << "...";
+        }
+        for (int x = 0; x < X - cell_size; x += cell_size) {
+            cell_roi.x = x;
+            cv::Mat grad_roi = grad(cell_roi);
+            cv::Mat angl_roi = angl(cell_roi);
+            cv::Mat h = HistCell(grad_roi, angl_roi);
+            h.copyTo(hist(hist_roi));
+            hist_roi.x++;
+        }
+        hist_roi.x = 0;
+        hist_roi.y++;
+    }
+    std::cout << " " << Y - cell_size << std::endl
+              << " finished histogram calculation" << std::endl;
+
+    // ブロックごとに規定化
+    std::cout << " start block calculation" << std::endl;
+    cv::Size hog_num(cell_num.width - block_size + 1,
+                     cell_num.height - block_size + 1);
+    cv::Rect roi(0, 0, block_size, block_size);
+    cv::Size hog_siz(block_area * hog_num.width, hog_num.height);
+    cv::Mat hog = cv::Mat::zeros(hog_siz, CV_32FC(bin_num));
+    cv::Rect hog_roi(0, 0, block_area, 1);
+    for (int y = 0; y < hog_num.height; y++) {
+        if (y % 100 == 0) {
+            std::cout << " " << y << "...";
+        }
+        for (int x = 0; x < hog_num.width; x++) {
+            cv::Mat r = hist(roi).clone();
+            cv::Mat rmat = r.reshape(1, 1);
+            double norm = 1.0 / (1.0 + cv::norm(rmat));
+            cv::Mat rn = r.mul(norm);
+            cv::Mat rnv = rn.reshape(bin_num, 1);
+            rnv.copyTo(hog(hog_roi));
+            roi.x++;
+            hog_roi.x += block_area;
+        }
+        roi.x = 0;
+        roi.y++;
+        hog_roi.x = 0;
+        hog_roi.y++;
+    }
+    std::cout << " " << hog_num.height << std::endl
+              << " finished block calculation" << std::endl;
+
+    // チャネル数、ビン数×ブロックサイズ^2、ブロック横×ブロック高さの Mat に変形
+    cv::Mat ret = hog.reshape(bin_num * block_area, hog_num.height);
+    std::cout << " reshape finished" << std::endl;
+
+    std::cout << "HOG finished, returns:" << ret.size() << ", ch=["
+              << ret.channels() << "]" << std::endl;
+    return ret;
+}
+
+int main(void) {
+    cv::Mat img = cv::imread("/home/amano/sample.jpg", 1);
+    if (img.empty()) return -1;
+
+    cv::Mat hog = HOG(img);
 
     cv::namedWindow("Image", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
-    cv::imshow("Image", angl);
+    cv::imshow("Image", img);
     cv::waitKey(0);
     return 0;
 }
